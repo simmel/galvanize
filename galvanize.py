3918bf71f8d547f61b6756b46932f7c1ec00ba3b
Simon Lundström <github-commits@soy.se>
2023-07-11 10:24:35 +0200

    Add meeting detector
    Also make sure our features complete and handle any exceptions that they
    throw.

--- galvanize.py
+++ galvanize.py
@@ -97,16 +97,91 @@ def set_led(should_shine, leds):
                 DEVICE.write([1, led, 0, 0, 0, 255, 255, 255])
 
 
+def meeting_detector():
+    global IN_MEETING
+    logger.info("MeetingDetector started")
+    threading.current_thread().name = (
+        sys._getframe().f_code.co_name  # pylint: disable=protected-access
+    )
+
+    class MeetingDetector(Automaton):
+        # pylint: disable=raising-bad-type
+        def master_filter(self, pkt):
+            should_accept = UDP in pkt and pkt[UDP].dport >= 8801 and pkt[UDP].dport <= 8810
+            pkt = None
+            return should_accept
+
+        @ATMT.state(initial=1)
+        def begin(self):
+            logger.debug("State=BEGIN")
+            raise self.waiting()
+
+        @ATMT.state()
+        def waiting(self):
+            logger.debug("in_meeting? %r", IN_MEETING)
+
+        @ATMT.receive_condition(waiting)
+        def chk_receive_data(self, pkt):
+            raiser = self.receiving(pkt)
+            pkt = None
+            raise raiser
+
+        # RECEIVED
+        @ATMT.state()
+        def receiving(self, pkt):
+            global IN_MEETING
+            IN_MEETING = True
+            try:
+                set_led(IN_MEETING, [3, 4, 5])
+            except (OSError, AttributeError):
+                logger.exception("Lost device: %r", DEVICE)
+                MeetingDetector().destroy()
+                MeetingDetector().run()
+            pkg = None
+            raise self.waiting()
+
+        @ATMT.timeout(waiting, 5)
+        def timeout_waiting(self):
+            global IN_MEETING
+            if DEVICE and IN_MEETING:
+                IN_MEETING = False
+                try:
+                    set_led(IN_MEETING, [3, 4, 5])
+                except (OSError, AttributeError):
+                    logger.exception("Lost device: %r", DEVICE)
+                    MeetingDetector().destroy()
+                    MeetingDetector().run()
+            raise self.waiting()
+
+        @ATMT.action(timeout_waiting)
+        def retransmit_last_packet(self):
+            logger.debug("hit timeout?")
+            logger.debug(IN_MEETING)
+
+        @ATMT.state(final=1)
+        def end(self):
+            logger.debug("State=END")
+
+    MeetingDetector().run()
+
+
 def main() -> None:
     logger.info("Starting up")
     with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
-        try:
-            while True:
-                executor.submit(discovery).result()
-                executor.submit(worker).result()
-        except KeyboardInterrupt:
-            logger.info("Shutting down")
-            os._exit(0)  # pylint: disable=protected-access
+        executor.submit(meeting_detector)
+        while True:
+            try:
+                futures = []
+                futures.append(executor.submit(discovery))
+                futures.append(executor.submit(worker))
+                for future in concurrent.futures.as_completed(futures):
+                    try:
+                        logger.debug("Future result: %r", future.result())
+                    except Exception:  # pylint: disable=broad-except
+                        logger.exception("Exception in thread")
+            except KeyboardInterrupt:
+                logger.info("Shutting down")
+                os._exit(0)  # pylint: disable=protected-access
 
 
 if __name__ == "__main__":
